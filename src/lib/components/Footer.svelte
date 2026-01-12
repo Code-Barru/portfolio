6a8601ff6c784c8362a16f1eb7f7e1f42628629d
Code-Barru <antoine.ousselin@protonmail.com>
2026-01-12 10:29:23 +0100

    chore: update copyright year

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 <div
 	class="text-mocha-subtext0 my-2 flex flex-row justify-end gap-2 text-sm"
 >
-	© 2025
+	© 2025-2026
 	<a
 		href="/"
 		class="hover:text-mocha-blue transitions-colors duration-200"
